297ed72eeaa0bd9d3a835c6047ca3fb4398c901b
Zach Stearman <zstearman3@gmail.com>
2019-04-23 21:35:08 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'help',    to: 'static_pages#help'
-  get '/signup', to: 'users#new'
+  get 'help',     to: 'static_pages#help'
+  get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :conferences
   resources :users
 end
