4ecf236a2578eb602720bec6f15d1729be829695
Zach Stearman <zstearman3@gmail.com>
2019-04-23 20:23:34 +0000

    Add static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get 'help',    to: 'static_pages#help'
+  get '/signup', to: 'users#new'
   resources :conferences
   resources :users
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | ProphetRatings"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
