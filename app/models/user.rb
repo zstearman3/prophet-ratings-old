161301d4150b73bc0150e51a21c4acbe458b1db2
Zach Stearman <zstearman3@gmail.com>
2019-04-24 20:03:18 +0000

    Add persistent cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save {self.email = email.downcase}
     validates :first_name, presence: true, length: { maximum: 50 }
     validates :last_name, presence: true, length: { maximum: 50 }
@@ -14,5 +15,20 @@ class User < ApplicationRecord
        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                      BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+    
+    # Returns a random token.
+    def User.new_token
+       SecureRandom.urlsafe_base64 
+    end
+    
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+    def authenticated?(remember_token)
+       BCrypt::Password.new(remember_digest).is_password?(rememebr_token) 
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190423162841) do
+ActiveRecord::Schema.define(version: 20190424192522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190423162841) do
     t.datetime "updated_at", null: false
     t.string "last_name"
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
