70e7163bb940398d0b68904e55dfc85f883e2e93
Zach Stearman <zstearman3@gmail.com>
2019-04-25 16:12:55 +0000

    Add flash to users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,14 +27,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated."
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
