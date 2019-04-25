844788204b6bf78c1e26dbea602a4c887e238cea
Zach Stearman <zstearman3@gmail.com>
2019-04-25 15:55:38 +0000

    Fix typo in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'hidden-cove-12485.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
