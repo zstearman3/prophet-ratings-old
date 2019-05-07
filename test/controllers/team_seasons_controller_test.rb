05815f5cffaf995e838eedbd94e5f44901e0bbd4
Zach Stearman <zstearman3@gmail.com>
2019-05-07 20:44:05 +0000

    Add some team_seasons testing

--- test/controllers/team_seasons_controller_test.rb
+++ test/controllers/team_seasons_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class TeamSeasonsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+  
+  test "should get index" do
+    get team_seasons_path(season: '2019')
+    assert_response :success
+  end
 end
--- test/models/team_season_test.rb
+++ test/models/team_season_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TeamSeasonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @season = team_seasons(:one)
+  end
+  
+  test "should be valid" do
+    assert @season.valid?
+  end
+  
+  test "should require team" do
+    @season.team_id = nil
+    assert_not @season.valid?
+  end
 end
