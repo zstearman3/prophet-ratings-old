b3226b62b94d0cbd207c5209a5bb28e6b05fd3bc
Zach Stearman <zstearman3@gmail.com>
2019-05-15 04:00:22 +0000

    Optional teams to games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
-  belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
-  belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
+  belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id', optional: true
+  belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id', optional: true
   belongs_to :season
+  validates :status, presence: true
+  validates :day, presence: true
 end
