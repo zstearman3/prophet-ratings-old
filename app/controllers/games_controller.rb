057caba8f88dd30c98bcc8012d877f4b844b5f0e
Zach Stearman <zstearman3@gmail.com>
2019-05-21 21:07:52 +0000

    Add box score to games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,7 +8,9 @@ class GamesController < ApplicationController
     @away_team_game = @game.team_games.find_by(home_or_away: 'AWAY')
     @home_team_game = @game.team_games.find_by(home_or_away: 'HOME')
     @away_team = @away_team_game.team
+    @home_team = @home_team_game.team
     @away_player_games = @game.player_games.where(team: @away_team)
+    @home_player_games = @game.player_games.where(team: @home_team)
   end
   
   private 
