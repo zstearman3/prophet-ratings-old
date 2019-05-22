587e6ed14c9e201e4b3355858165a396596526be
Zach Stearman <zstearman3@gmail.com>
2019-05-22 21:29:07 +0000

    Add stylings and functionality to show views

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,7 @@
 class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
+    @player_seasons = PlayerSeason.where(player: @player).order(year: :desc)
+    @player_games = @player.player_games.order(day: :desc).first(5)
   end
 end
