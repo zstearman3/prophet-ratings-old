5eaa70c0ac51f3024c7548da670ac88c547f2ca9
Zach Stearman <zstearman3@gmail.com>
2019-06-27 21:22:34 +0000

    Add passing test suite

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,7 @@ class Game < ApplicationRecord
   has_many :team_games
   has_many :player_games
   has_one  :prediction
-  belongs_to :player_of_the_game, :class_name => 'Player', :foreign_key => 'player_of_the_game_id'
+  belongs_to :player_of_the_game, :class_name => 'Player', :foreign_key => 'player_of_the_game_id', optional: true
   validates :status, presence: true
   validates :day, presence: true
 end
