f27ed96b46983b9b391a38f4b7890f7186ca9711
Zach Stearman <zstearman3@gmail.com>
2019-06-27 16:22:35 +0000

    Add prating show to player games all through app

--- app/controllers/player_games_controller.rb
+++ app/controllers/player_games_controller.rb
@@ -1,8 +1,9 @@
 class PlayerGamesController < ApplicationController
   def index
     @player = Player.find(params[:player])
-    @previous_season = Season.find_by(season: current_season.season - 1)
-    @games = PlayerGame.where(player: @player, season: current_season).order(day: :desc)
-    @oldgames = PlayerGame.where(player: @player, season: @previous_season).order(day: :desc)
+    @season = Season.find_by(season: params[:year]) if params[:year]
+    @season ||= current_season
+    @previous_season = Season.find_by(season: @season.season - 1)
+    @games = PlayerGame.where(player: @player, season: @season).order(day: :desc)
   end
 end
