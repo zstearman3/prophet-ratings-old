bcc89539b67b6d6ed35ea6baac1d8780cb3b5bf3
Zach Stearman <zstearman3@gmail.com>
2019-07-16 03:47:43 +0000

    Redirect to dashboard on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or rankings_path
+        redirect_back_or dashboard_path
       else
         message = "Account not activated. Check your email for the activation link."
         flash[:warning] = message
