7a21f02bd0c0a6e9574efa5e60943a9596e8ee83
Zach Stearman <zstearman3@gmail.com>
2019-07-22 18:32:05 +0000

    Custom Error Pages

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ Rails.application.routes.draw do
   get '/blog',               to: 'blog_posts#index',             as: :blog_posts
   get '/preseason',          to: 'teams#preseason',              as: :preseason
   get '/player_preseason',   to: 'player_seasons#preseason',     as: :player_preseason
+  get '/404',                to: 'errors#not_found'
+  get '/422',                to: 'errors#unacceptable'
+  get '/500',                to: 'errors#internal_error'
   resources :conferences
   resources :users
   resources :account_activations, only: [:edit]
