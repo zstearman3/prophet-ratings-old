330db8f977ca4de4311d472f6291869d205f5a8e
Zach Stearman <zstearman3@gmail.com>
2019-07-23 20:16:16 +0000

    Add mid majors to team sorting

--- app/controllers/team_seasons_controller.rb
+++ app/controllers/team_seasons_controller.rb
@@ -4,38 +4,82 @@ class TeamSeasonsController < ApplicationController
   
   def shooting
     @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id]
-      @team_seasons = @team_seasons.where(conference_id: params[:conference_id]).order("#{sort_column} #{sort_direction}")
-    else
-      @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    if params[:conference_id].to_i > 0
+      if params[:conference_id].to_i == 50
+        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      elsif params[:conference_id].to_i == 40
+        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      else
+        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+      end
     end
+    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    @conferences = Conference.order(name: :asc).all.as_json
+    @conferences << {"id" => 40, "name" => "Powers"}
+    @conferences << {"id" => 50, "name" => "Mid-Majors"}
+    @conferences.to_json 
   end
   
   def defense
     @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id]
-      @team_seasons = @team_seasons.where(conference_id: params[:conference_id]).order("#{sort_column} #{sort_direction}")
-    else
-      @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    if params[:conference_id].to_i > 0
+      if params[:conference_id].to_i == 50
+        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      elsif params[:conference_id].to_i == 40
+        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      else
+        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+      end
     end
+    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    @conferences = Conference.order(name: :asc).all.as_json
+    @conferences << {"id" => 40, "name" => "Powers"}
+    @conferences << {"id" => 50, "name" => "Mid-Majors"}
+    @conferences.to_json 
   end
   
   def rebounding
     @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id]
-      @team_seasons = @team_seasons.where(conference_id: params[:conference_id]).order("#{sort_column} #{sort_direction}")
-    else
-      @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    if params[:conference_id].to_i > 0
+      if params[:conference_id].to_i == 50
+        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      elsif params[:conference_id].to_i == 40
+        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      else
+        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+      end
     end
+    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    @conferences = Conference.order(name: :asc).all.as_json
+    @conferences << {"id" => 40, "name" => "Powers"}
+    @conferences << {"id" => 50, "name" => "Mid-Majors"}
+    @conferences.to_json 
   end
   
   def miscellaneous
     @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id]
-      @team_seasons = @team_seasons.where(conference_id: params[:conference_id]).order("#{sort_column} #{sort_direction}")
-    else
-      @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    if params[:conference_id].to_i > 0
+      if params[:conference_id].to_i == 50
+        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      elsif params[:conference_id].to_i == 40
+        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      else
+        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+      end
     end
+    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    @conferences = Conference.order(name: :asc).all.as_json
+    @conferences << {"id" => 40, "name" => "Powers"}
+    @conferences << {"id" => 50, "name" => "Mid-Majors"}
+    @conferences.to_json 
   end
   
   def rankings
@@ -44,10 +88,22 @@ class TeamSeasonsController < ApplicationController
     else
       @team_seasons = TeamSeason.where(year: current_season.season)
     end
-    if params[:conference_id]
-      @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+    if params[:conference_id].to_i > 0
+      if params[:conference_id].to_i == 50
+        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      elsif params[:conference_id].to_i == 40
+        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
+        @team_seasons = @team_seasons.where(conference_id: conferences_list)
+      else
+        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
+      end
     end
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+    @conferences = Conference.order(name: :asc).all.as_json
+    @conferences << {"id" => 40, "name" => "Powers"}
+    @conferences << {"id" => 50, "name" => "Mid-Majors"}
+    @conferences.to_json 
   end
   
   private 
