2a6ba129be9d3d85c691f4be9cbc9cd19de5efde
Zach Stearman <zstearman3@gmail.com>
2019-09-27 14:11:12 +0000

    Add invisible captcha

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 gem 'font-awesome-sass',       '~> 5.8.1'
 gem 'trix-rails',      require: 'trix'
+gem 'invisible_captcha'
 
 group :development do
   gem 'web-console',           '3.5.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  invisible_captcha only: [:create, :update], honeypot: :robots
 
   def index
     @users = User.all
