2dc8adab0ed01d65e5c332c87f3371c3636ed21e
Zach Stearman <zstearman3@gmail.com>
2020-07-21 19:28:16 +0000

    Add current season and rankings_to_hash methods to Seasons

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -2,17 +2,13 @@ class Ranking < ApplicationRecord
   before_save :get_full_rankings
   serialize :full_rankings
   
-  def team_seasons
-    TeamSeason.where(season: Season.last)
+  def current_hash
+    Season.current_season.rankings_to_hash
   end
   
   private
     def get_full_rankings
-      rankings_hash = {}
-      team_seasons.each do |season|
-        rankings_hash[season.id] = season.adj_efficiency_margin
-      end
-      self.full_rankings = rankings_hash
+      self.full_rankings = current_hash
       self.date = Date.today
     end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,4 +1,5 @@
 class Season < ApplicationRecord
+  CURRENT_YEAR = 2020
   has_many :team_seasons
   has_many :player_seasons
   has_many :team_games
@@ -7,4 +8,16 @@ class Season < ApplicationRecord
   validates :season, presence: true, uniqueness: true
   validates :regular_season_start_date, presence: true
   validates :post_season_end_date, presence: true
+  
+  def self.current_season
+    Season.find_by(season: CURRENT_YEAR)
+  end
+  
+  def rankings_to_hash
+    rankings_hash = {}
+    team_seasons.each do |team_season| 
+      rankings_hash[team_season.id] = team_season.adj_efficiency_margin
+    end
+    rankings_hash
+  end
 end
