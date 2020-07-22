dce13dc371a9fa696c145fb23ec6291461ee3f9f
Zach Stearman <zstearman3@gmail.com>
2020-07-22 02:25:45 +0000

    Fully DRY team_seasons_controller

--- app/controllers/team_seasons_controller.rb
+++ app/controllers/team_seasons_controller.rb
@@ -1,58 +1,35 @@
 class TeamSeasonsController < ApplicationController
   before_action :logged_in_user, only: [:shooting, :defense, :rebounding, :miscellaneous]
+  before_action :set_instance_variables
   helper_method :sort_column, :sort_direction
   
   def shooting
-    @team_seasons = TeamSeason.where(year: year)
-    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
-    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
-    @conferences = Conference.order(name: :asc).all.as_json
-    @conferences << {"id" => 40, "name" => "Powers"}
-    @conferences << {"id" => 50, "name" => "Mid-Majors"}
-    @conferences.to_json 
   end
   
   def defense
-    @team_seasons = TeamSeason.where(year: year)
-    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
-    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
-    @conferences = Conference.order(name: :asc).all.as_json
-    @conferences << {"id" => 40, "name" => "Powers"}
-    @conferences << {"id" => 50, "name" => "Mid-Majors"}
-    @conferences.to_json 
   end
   
   def rebounding
-    @team_seasons = TeamSeason.where(year: year)
-    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
-    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
-    @conferences = Conference.order(name: :asc).all.as_json
-    @conferences << {"id" => 40, "name" => "Powers"}
-    @conferences << {"id" => 50, "name" => "Mid-Majors"}
-    @conferences.to_json 
   end
   
   def miscellaneous
-    @team_seasons = TeamSeason.where(year: year)
-    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
-    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
-    @conferences = Conference.order(name: :asc).all.as_json
-    @conferences << {"id" => 40, "name" => "Powers"}
-    @conferences << {"id" => 50, "name" => "Mid-Majors"}
-    @conferences.to_json 
   end
   
   def rankings
-    @team_seasons = TeamSeason.where(year: year)
-    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
-    @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
-    @conferences = Conference.order(name: :asc).all.as_json
-    @conferences << {"id" => 40, "name" => "Powers"}
-    @conferences << {"id" => 50, "name" => "Mid-Majors"}
-    @conferences.to_json 
   end
   
   private 
+  
+    def set_instance_variables
+      @team_seasons = TeamSeason.where(year: year)
+      @team_seasons = @team_seasons.where(conference_id: conference_ids) if conference_ids
+      @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
+      @conferences = Conference.order(name: :asc).all.as_json
+      @conferences << {"id" => 40, "name" => "Powers"}
+      @conferences << {"id" => 50, "name" => "Mid-Majors"}
+      @conferences.to_json 
+    end
+    
     def sortable_columns
       ['name', 'wins', 'adjem_rank', 'adj_offensive_efficiency', 'adj_defensive_efficiency', 'adj_tempo', 
        'field_goals_percentage', 'effective_field_goals_percentage', 'three_pointers_percentage', 'assists_percentage',
@@ -77,7 +54,7 @@ class TeamSeasonsController < ApplicationController
       params[:season] ? params[:season] : Season.current_year
     end
     
-    def conferences
+    def conference_ids
       return nil if (params[:conference_id].to_i == 0)
       if params[:conference_id].to_i == 50
         major_conferences
