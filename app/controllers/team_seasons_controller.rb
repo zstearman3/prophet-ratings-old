06c5acdba6ea89a5aa31530d420d707701b048f5
Zach Stearman <zstearman3@gmail.com>
2020-07-22 02:15:33 +0000

    DRY team_seasons_controller

--- app/controllers/team_seasons_controller.rb
+++ app/controllers/team_seasons_controller.rb
@@ -3,18 +3,8 @@ class TeamSeasonsController < ApplicationController
   helper_method :sort_column, :sort_direction
   
   def shooting
-    @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id].to_i > 0
-      if params[:conference_id].to_i == 50
-        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      elsif params[:conference_id].to_i == 40
-        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      else
-        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
-      end
-    end
+    @team_seasons = TeamSeason.where(year: year)
+    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
     @conferences = Conference.order(name: :asc).all.as_json
     @conferences << {"id" => 40, "name" => "Powers"}
@@ -23,18 +13,8 @@ class TeamSeasonsController < ApplicationController
   end
   
   def defense
-    @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id].to_i > 0
-      if params[:conference_id].to_i == 50
-        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      elsif params[:conference_id].to_i == 40
-        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      else
-        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
-      end
-    end
+    @team_seasons = TeamSeason.where(year: year)
+    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
     @conferences = Conference.order(name: :asc).all.as_json
     @conferences << {"id" => 40, "name" => "Powers"}
@@ -43,18 +23,8 @@ class TeamSeasonsController < ApplicationController
   end
   
   def rebounding
-    @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id].to_i > 0
-      if params[:conference_id].to_i == 50
-        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      elsif params[:conference_id].to_i == 40
-        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      else
-        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
-      end
-    end
+    @team_seasons = TeamSeason.where(year: year)
+    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
     @conferences = Conference.order(name: :asc).all.as_json
     @conferences << {"id" => 40, "name" => "Powers"}
@@ -63,18 +33,8 @@ class TeamSeasonsController < ApplicationController
   end
   
   def miscellaneous
-    @team_seasons = TeamSeason.where(year: params[:season])
-    if params[:conference_id].to_i > 0
-      if params[:conference_id].to_i == 50
-        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      elsif params[:conference_id].to_i == 40
-        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      else
-        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
-      end
-    end
+    @team_seasons = TeamSeason.where(year: year)
+    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
     @conferences = Conference.order(name: :asc).all.as_json
     @conferences << {"id" => 40, "name" => "Powers"}
@@ -83,22 +43,8 @@ class TeamSeasonsController < ApplicationController
   end
   
   def rankings
-    if params[:season]
-      @team_seasons = TeamSeason.where(year: params[:season])
-    else
-      @team_seasons = TeamSeason.where(year: current_season.season)
-    end
-    if params[:conference_id].to_i > 0
-      if params[:conference_id].to_i == 50
-        conferences_list = [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      elsif params[:conference_id].to_i == 40
-        conferences_list = [1, 3, 6, 9, 10, 15, 24, 26]
-        @team_seasons = @team_seasons.where(conference_id: conferences_list)
-      else
-        @team_seasons = @team_seasons.where(conference_id: params[:conference_id])
-      end
-    end
+    @team_seasons = TeamSeason.where(year: year)
+    @team_seasons = @team_seasons.where(conference_id: conferences) if conferences
     @team_seasons = @team_seasons.order("#{sort_column} #{sort_direction}")
     @conferences = Conference.order(name: :asc).all.as_json
     @conferences << {"id" => 40, "name" => "Powers"}
@@ -125,5 +71,28 @@ class TeamSeasonsController < ApplicationController
     def sort_direction(init_direction = nil)
       init_direction ||= "asc"
       %w[asc desc].include?(params[:direction]) ? params[:direction] : init_direction
+    end
+    
+    def year
+      params[:season] ? params[:season] : Season.current_year
+    end
+    
+    def conferences
+      return nil if (params[:conference_id].to_i == 0)
+      if params[:conference_id].to_i == 50
+        major_conferences
+      elsif params[:conference_id].to_i == 40
+        mid_major_conferences
+      else
+        params[:conference_id]
+      end
+    end
+    
+    def major_conferences
+      [2, 4, 5, 7, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 31, 32, 33]
+    end
+    
+    def mid_major_conferences
+      [1, 3, 6, 9, 10, 15, 24, 26]
     end
 end
