f60617f7d71efb2263788f6bf78393caf1b32d40
Zach Stearman <zstearman3@gmail.com>
2020-07-22 03:50:24 +0000

    Pull logic out of rankings view

--- app/models/team_season.rb
+++ app/models/team_season.rb
@@ -3,4 +3,20 @@ class TeamSeason < ApplicationRecord
   belongs_to :season, optional: true
   belongs_to :conference, optional: true
   has_many :player_seasons
+  
+  def school
+    team.school
+  end
+  
+  def conference
+    team.conference
+  end
+  
+  def conference_name
+    team.conference.name
+  end
+  
+  def conference_abbr
+    team.conference.abbreviation
+  end
 end
